2f84de35f78e5e28936c45db5a26974385056001
rena0204 <rena0204@sookmyung.ac.kr>
2025-11-02 23:54:41 +0900

    09 (3)

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 
 int main(int argc, char *argv[])
 {
-	int testscore[SIZE];
 	int i;
-	
-	printf("%d명 점수를 입력하세요\n", SIZE);
+	int a[SIZE]={1,2,3,4,5};
+	int b[SIZE]={1,2,3,4,5};
+	int flag_same=1;
 	
 	for(i=0;i<SIZE;i++)
-		scanf("%d",&testscore[i]);
-		
-	for(i=0;i<SIZE;i++)
-		printf("grade[%d]=%d\n",i, testscore[i]);
+	{
+		if(a[i]!=b[i])
+		{
+			printf("%i-th element is diff.\n",i);
+			flag_same=0;
+		}
+	}
+	
+	printf("Are a and b the same? %i\n",flag_same);
+	
+	return 0;
 }
 
 
